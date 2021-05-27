363eab3e6a3d65344e2e02cb777091bcb0e8d5aa
Andrew Dotterer <adotterer@gmail.com>
2021-05-27 13:14:19 -0700

    dates

--- weather_getter.py
+++ weather_getter.py
@@ -17,9 +17,15 @@ API_KEY = os.environ.get("OPEN_WEATHER_API_KEY")
 # with open("./json/weather.json", "w") as outfile:
 #     json.dump(json_data, outfile, indent=4)
 
-weather_dict = json.load(open("./json/weather.json","r"))
+weather_dict = json.load(open("./json/weather.json", "r"))
 
-print(weather_dict["daily"])
+# print(weather_dict["daily"])
+
+for day in weather_dict["daily"]:
+    print(
+        datetime.utcfromtimestamp(
+            day['dt']).strftime('%Y-%m-%d'))
+
 
 # y = json.loads(json_data)
 # print(json_data["daily"])
