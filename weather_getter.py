481be438fc456bcecb7ca9a5da1bc53db8dcc583
Andrew Dotterer <adotterer@gmail.com>
2021-05-27 11:53:13 -0700

    indent to make it readable

--- weather_getter.py
+++ weather_getter.py
@@ -8,8 +8,10 @@ API_KEY = os.environ.get("OPEN_WEATHER_API_KEY")
 
 # print(API_KEY)
 response = requests.get(
-    f"https://api.openweathermap.org/data/2.5/onecall?lat=48&lon=-122&cnt=5&exlude=minutely,hourly&appid={API_KEY}"
+    f"https://api.openweathermap.org/data/2.5/onecall?lat=48&lon=-122&cnt=5&exclude=minutely,hourly&appid={API_KEY}"
 )
 
+json_data = response.json()
+
 with open("./json/weather.json", "w") as outfile:
-    json.dump(response.json(), outfile)
+    json.dump(json_data, outfile, indent=4)
